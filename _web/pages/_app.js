04d37d396541f14293b8f109f2842c668a07eb32
Dendi Handian <handian.dendi@gmail.com>
2021-11-14 15:13:54 +0700

    setup tailwind

--- _web/pages/_app.js
+++ _web/pages/_app.js
@@ -1,7 +1,15 @@
 import '../styles/globals.css'
 
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return (
+    <div className="wrapper">
+      <header></header>
+      <main>
+        <Component {...pageProps} />
+      </main>
+      <footer></footer>
+    </div>
+  )
 }
 
 export default MyApp
