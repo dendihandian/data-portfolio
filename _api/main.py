b8770b9ec36c9a3522d8bf86b939977edccd67f6
Dendi Handian <handian.dendi@gmail.com>
2021-11-14 21:16:13 +0700

    add cors, add axios for web

--- _api/main.py
+++ _api/main.py
@@ -1,11 +1,24 @@
 from typing import Optional
-
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
 from pydantic import BaseModel
 import pandas as pd
 import pickle
 
 app = FastAPI()
+
+origins = [
+    "http://localhost:3000",
+    "https://data-portfolio.netlify.app",
+]
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 class BankMarketingPredict(BaseModel):
     age: int
