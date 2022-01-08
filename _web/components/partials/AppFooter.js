5f330431fc9f0986cd3acbb660a0ec90fb0950cf
Dendi Handian <contact.dendi@yahoo.com>
2022-01-08 22:46:01 +0700

    add web icon for homepage site

--- _web/components/partials/AppFooter.js
+++ _web/components/partials/AppFooter.js
@@ -8,6 +8,11 @@ export default function AppFooter() {
                 <p className="hidden mx-1 md:block">-</p>
                 <p className="mt-4 text-sm text-gray-500 md:py-2 md:mt-0">Icons by <a className="hover:text-purple-500" href="https://heroicons.com" target="_blank" rel="noreferrer">Heroicons</a> & <a className="hover:text-blue-500" href="https://fontawesome.com/license" target="_blank" rel="noreferrer">Font Awesome</a></p>
                 <span className="inline-flex justify-center mt-4 md:ml-auto md:mt-0 md:justify-start">
+                    <a className="ml-3 text-gray-500" href="https://dendihandian.github.io" target="_blank" rel="noreferrer">
+                        <svg xmlns="http://www.w3.org/2000/svg" className="w-5 h-5 svg-inline--fa fa-dev fa-w-14" fill="none" viewBox="0 0 24 24" stroke="currentColor">
+                            <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M21 12a9 9 0 01-9 9m9-9a9 9 0 00-9-9m9 9H3m9 9a9 9 0 01-9-9m9 9c1.657 0 3-4.03 3-9s-1.343-9-3-9m0 18c-1.657 0-3-4.03-3-9s1.343-9 3-9m-9 9a9 9 0 019-9" />
+                        </svg>
+                    </a>
                     <a className="ml-3 text-gray-500" href="https://dev.to/dendihandian" target="_blank" rel="noreferrer">
                         <svg aria-hidden="true" focusable="false" data-prefix="fab" data-icon="dev" className="w-5 h-5 svg-inline--fa fa-dev fa-w-14" role="img" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 448 512">
                             <path fill="currentColor" d="M120.12 208.29c-3.88-2.9-7.77-4.35-11.65-4.35H91.03v104.47h17.45c3.88 0 7.77-1.45 11.65-4.35 3.88-2.9 5.82-7.25 5.82-13.06v-69.65c-.01-5.8-1.96-10.16-5.83-13.06zM404.1 32H43.9C19.7 32 .06 51.59 0 75.8v360.4C.06 460.41 19.7 480 43.9 480h360.2c24.21 0 43.84-19.59 43.9-43.8V75.8c-.06-24.21-19.7-43.8-43.9-43.8zM154.2 291.19c0 18.81-11.61 47.31-48.36 47.25h-46.4V172.98h47.38c35.44 0 47.36 28.46 47.37 47.28l.01 70.93zm100.68-88.66H201.6v38.42h32.57v29.57H201.6v38.41h53.29v29.57h-62.18c-11.16.29-20.44-8.53-20.72-19.69V193.7c-.27-11.15 8.56-20.41 19.71-20.69h63.19l-.01 29.52zm103.64 115.29c-13.2 30.75-36.85 24.63-47.44 0l-38.53-144.8h32.57l29.71 113.72 29.57-113.72h32.58l-38.46 144.8z"></path>
